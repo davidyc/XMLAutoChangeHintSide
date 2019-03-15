00d49480eb98093bb2bde37be3c666b6ac9d1a19
davidyc <davydovsergey.sd@gmail.com>
2019-03-15 22:29:41 +0600

    add os

--- parser.py
+++ parser.py
@@ -1,4 +1,16 @@
 import xml.etree.ElementTree as ET
+import os  
+
+  '''  f = open( "pages/xml.xml",'rt')
+	data = xml.dom.minidom.parseString(f.read())
+	f.close()
+	element = xml.dom.minidom.Element('id')
+	element.setAttribute('value','newAttr')
+	f = open("pages/xml.xml",'wt')
+	qw = data.toxml()
+	qw = qw.encode('utf-8')
+	f.write(qw)
+	f.close()'''
 
 tree = ET.parse('1.xaml')
 root = tree.getroot()
@@ -8,12 +20,20 @@ element = elements.find('{http://schemas.microsoft.com/netfx/2010/xaml/activitie
 
 
 print(element)
+mykey = '{http://schemas.microsoft.com/netfx/2009/xaml/activities/presentation}VirtualizedContainerService.HintSize'
 
 for i in element:
-    print(i.attrib['{http://schemas.microsoft.com/netfx/2009/xaml/activities/presentation}VirtualizedContainerService.HintSize'])
+    print(i.attrib[mykey])
 
 
+for i in element:
+     if i.attrib[mykey] == '314,60':
+         print("Yes")
+         i.attrib[mykey] = '00,00'
 
+
+for i in element:
+    print(i.attrib[mykey])
 
 
 
