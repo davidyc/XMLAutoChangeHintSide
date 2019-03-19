763324418d5b5087a515908ee160f7d40e1ae706
Sergey Davydov2 <Sergey_Davydov2@epam.com>
2019-03-19 11:02:23 +0600

    add GUI

--- FixXAML.py
+++ FixXAML.py
@@ -1,3 +1,26 @@
 import FuncFixXAML as FFX
+from tkinter import *
+from tkinter import messagebox
 
-FFX.FixXAML("1.xaml", "2.xaml")
+
+def CallFunc():
+    messagebox.showinfo("Info", "Done!")
+
+root = Tk()
+root.title("Fix XAML")
+root.geometry('600x130')
+
+Label(text="New:").grid(row=0, column=0, sticky=W, padx=10, pady=10) 
+Entry(width="86", name="newFile").grid(row=0, column=1) 
+Label(text="Old:").grid(row=1, column=0, sticky=W, padx=10, pady=10)
+Entry(width="86", name="oldFile").grid(row=1, column=1) 
+
+
+Button(text="Fix", command=CallFunc, width=20, height=1).grid(row=2, column=1)
+
+
+
+
+
+
+root.mainloop()
